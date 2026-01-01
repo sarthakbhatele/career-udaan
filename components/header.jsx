8a3f7727e1446948dc262e3d1e6654c4198a0af6
sarthakbhatele <sarthakbhatele0208@gmail.com>
2026-01-02 04:18:54 +0530

    added new logo and enhanced the headeer btns

--- components/header.jsx
+++ components/header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button } from "./ui/button";
+import { Button as MovingBorderButton } from "./ui/moving-border";
 import {
   PenBox,
   LayoutDashboard,
@@ -27,11 +28,11 @@ export default async function Header() {
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/logo.png"}
+            src={"/logo3.png"}
             alt="Career Udaan Logo"
-            width={200}
-            height={40}
-            className="h-16 py-1 w-auto object-contain animate-glow"
+            width={280}
+            height={60}
+            className="h-12 sm:h-14 md:h-16 py-1 w-auto object-contain animate-glow"
           />
         </Link>
 
@@ -39,25 +40,25 @@ export default async function Header() {
         <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
             <Link href="/dashboard">
-              <Button
-                variant="outline"
-                className="hidden md:inline-flex items-center gap-2"
+              <MovingBorderButton
+                borderRadius="0.75rem"
+                duration={4000}
+                containerClassName="inline-flex h-10 w-10 md:w-auto"
+                className="bg-black border-slate-700/30 px-0 md:px-4 gap-2 justify-center"
+                borderClassName="bg-[radial-gradient(#c5c7cc_40%,transparent_60%)]"
               >
                 <LayoutDashboard className="h-4 w-4" />
-                Industry Insights
-              </Button>
-              <Button variant="ghost" className="md:hidden w-10 h-10 p-0">
-                <LayoutDashboard className="h-4 w-4" />
-              </Button>
+                <span className="hidden md:inline">Industry Insights</span>
+              </MovingBorderButton>
             </Link>
 
             {/* Growth Tools Dropdown */}
             <DropdownMenu>
               <DropdownMenuTrigger asChild>
-                <Button className="flex items-center gap-2">
-                  <StarsIcon className="h-4 w-4" />
+                <Button className="flex items-center gap-2 md:gap-2 h-9 w-10 md:w-auto md:px-4 p-0 md:p-2 rounded-xl">
+                  <StarsIcon className="h-4 w-4 hidden md:block" />
                   <span className="hidden md:block">Growth Tools</span>
-                  <ChevronDown className="h-4 w-4" />
+                  <ChevronDown className="h-4 w-4 md:ml-0" />
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent align="end" className="w-48">
